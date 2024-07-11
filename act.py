296f6491943f19983cd829abc10045a86cc08e09
Lowell Javier <136763472+Lviers@users.noreply.github.com>
2024-07-11 00:48:20 -0700

    Add files via upload

---
+++ act.py
@@ -0,0 +1,31 @@
+#custom tokenization
+"""input  = input("Enter yo number and alpha: ")
+stwing = ""
+mylist = []
+special_characters = [",",".","?",","]
+
+
+for x in input + "":  
+	if x.isalpha() or x.isalnum():
+		stwing += x
+	elif x in special_characters:
+		input += 
+		mylist.append(stwing)
+		stwing = ""
+	else:
+		mylist.append(stwing) 
+		stwing = ""
+print(mylist)"""
+
+#letter tokenization
+"""
+input  = input("Enter yo number and alpha: ")
+mylist = []
+
+for x in input :  
+		mylist.append(x)
+		
+print(mylist)
+"""
+
+#sentence tokenization
